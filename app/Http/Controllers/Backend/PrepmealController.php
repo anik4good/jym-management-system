107056b503c5360cf4f9b7f6cf8cfccd4a9c812a
Mustafa Munawar Anik <anik4good@gmail.com>
2021-06-03 16:46:32 +0600

    meal generated for 2 times fixed

--- app/Http/Controllers/Backend/PrepmealController.php
+++ app/Http/Controllers/Backend/PrepmealController.php
@@ -7,6 +7,7 @@ use App\Models\Food;
 use App\Models\Mealtime;
 use App\Models\Morning;
 use App\Models\Noon;
+use App\Models\Prepmeal;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
@@ -18,23 +19,45 @@ class PrepmealController extends Controller
      *
      * @return string
      */
-    public function index()
+//    public function index()
+//    {
+//        $foods = Food::limit(100)->get();
+//        $morning = Morning::all();
+//        $noon = Noon::all();
+//      return view('backend.meals.index',compact('foods','morning','noon'));
+//    }
+//    public function index($id)
+//    {
+//        $foods = Food::limit(100)->get();
+//        $morning = Morning::where('post_id',$id)->get();
+//        $noon = Noon::where('post_id',$id)->get();
+//
+//        return view('backend.meals.index',compact('foods','morning','noon'));
+//    }
+
+    public function index2($id)
     {
         $foods = Food::limit(100)->get();
-        $morning = Morning::all();
-        $noon = Noon::all();
-      return view('backend.meals.index',compact('foods','morning','noon'));
-    }
+        $morning = Morning::where('post_id',$id)->get();
+        $noon = Noon::where('post_id',$id)->get();
 
+        return view('backend.meals.index',compact('foods','morning','noon'));
+    }
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function create()
     {
         // start the backup process
-        Artisan::call('create:meal');
+        $prepmeal = new Prepmeal();
+        $prepmeal->user_id = 1;
+        $prepmeal->save();
+        Artisan::call('create:meal --calories=2000 --meals=3 --user_id=1 --user_meal_id='.$prepmeal->id);
+        //$this->index($prepmeal->id);
+         return redirect()->route('app.meals.update.time',$prepmeal->id);
+        //return redirect()->route('app.meals.index');
 
        // notify()->success('Backup Created Successfully.', 'Added');
        // return redirect()->route('app.meals.index');
@@ -57,26 +80,26 @@ class PrepmealController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-
-
-        return   Mealtime::where('food_id',$id)->first();
-
-        $mealtime = new Mealtime();
-         $mealtime->post_id=$prepmeal->id;
-        $mealtime->food_id=$id;
-        $mealtime->morning=1;
-        $mealtime->noon=0;
-        $mealtime->afternoon=0;
-        $mealtime->night=0;
-        $mealtime->save();
-        notify()->success('Meal Successfully Added.', 'Added');
-
-
-        return redirect()->route('app.meals.index');
-
-    }
+//    public function show($id)
+//    {
+//
+//
+//        return   Mealtime::where('food_id',$id)->first();
+//
+//        $mealtime = new Mealtime();
+//         $mealtime->post_id=$prepmeal->id;
+//        $mealtime->food_id=$id;
+//        $mealtime->morning=1;
+//        $mealtime->noon=0;
+//        $mealtime->afternoon=0;
+//        $mealtime->night=0;
+//        $mealtime->save();
+//        notify()->success('Meal Successfully Added.', 'Added');
+//
+//
+//        return redirect()->route('app.meals.index');
+//
+//    }
 
 
     public function show_post($food_id,$post_id,$check){
